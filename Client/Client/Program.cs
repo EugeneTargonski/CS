6ac627f1badfbcadb864ccb0adaaf2db96f7ef5c
eugene_targonski <tevstim@gmail.com>
2020-03-20 14:09:10 +0300

    .net Core 3.1

---
+++ Client/Client/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Net.Sockets;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string localAddr = "10.10.10.10";
+            int port = 8888;
+
+            var client = new TcpClient(localAddr, port);
+
+            string request = Console.ReadLine();
+
+            using (var bw = new BinaryWriter(client.GetStream()))
+                using (var br = new BinaryReader(client.GetStream()))
+                {
+                    bw.Write(request);
+                    switch (request)
+                    {
+                        case "stop":
+                            Console.WriteLine("Server stopped");
+                            break;
+                        default:
+                            Console.WriteLine(br.ReadString());
+                            break;
+                    }
+                }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Server/Server/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int port = 8888;
+
+            var listener = new TcpListener(IPAddress.Any, port);
+            listener.Start();
+            bool stop = false;
+            while (!stop)
+            {
+                var client = listener.AcceptTcpClient();
+                using (var br = new BinaryReader(client.GetStream()))
+                    using (var bw = new BinaryWriter(client.GetStream()))
+                    {
+                        var request = br.ReadString();
+                        Console.WriteLine(request);
+
+                        switch (request)
+                        {
+                            case "stop":
+                                stop = true;
+                                break;
+                            default:
+                                bw.Write("Привет " + request);
+                                break;
+                        }
+
+                    }
+            }
+        }
+    }
+}
